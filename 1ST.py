0cd29590271797de6cab6032b7d6c30b4cacb195
BARNAMAY98 <avinabaghosh5@gmail.com>
2025-05-30 22:21:08 +0530

    Add files via upload

---
+++ 1ST.py
@@ -0,0 +1,12 @@
+A=input("ENTER THE FIRST NUMBER")
+A=int(A)
+B=input("ENTER THE SECOND NUMBER")
+B=int(B)
+D=A+B
+print("ADDITION:", D)
+E=A-B
+print("SUBTRACTION:", E)
+G=A*B
+print("MULTIPLICATION:", G)
+H=A/B
+print("DIVISION:", H)
